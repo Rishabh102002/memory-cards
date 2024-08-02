43507fda1946594433b94fcfe2d0deb0e4bd0929
rishabh <rishu10102002@gmail.com>
2024-08-02 21:13:07 +0530

    made website woking on github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/memory-card/",
     plugins: [react()],
     css: {
         postcss: "./postcss.config.js",
