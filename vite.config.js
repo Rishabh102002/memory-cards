a2ef0e873f308d8bd2813f4f7482e177f1296567
rishabh <rishu10102002@gmail.com>
2024-08-02 21:16:37 +0530

    edited vite's config file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/memory-card/",
+    base: "/memory-cards/",
     plugins: [react()],
     css: {
         postcss: "./postcss.config.js",
